1006717b0c2c6915f678d1d8c3a46d61b46f2e2a
kazuuud <jinmakazuhiro@outlook.jp>
2024-11-07 15:28:10 +0900

    教員ログイン機能実装（正常系）

--- src/main/java/com/example/sswms/controller/Register.java
+++ src/main/java/com/example/sswms/controller/Register.java
@@ -1,16 +1,46 @@
 package com.example.sswms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequestMapping("/")
 public class Register {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("register")
     public String getMethodName() {
         return "teacher-register";
     }
+
+    @PostMapping("create-teacher")
+    public String postMethodName(@RequestParam("name") String name, @RequestParam("email") String email, @RequestParam("password") String password) {
+        
+        System.out.println( name );
+        System.out.println( email );
+        System.out.println( password );
+
+        try {
+
+            String sql = "INSERT INTO teacher values( ?, ? ,? )";
+            jdbcTemplate.update( sql , email, name, password );
+            
+            return "teacher-login";
+            
+        } catch (Exception e) {
+            
+            return "teacher-register";
+        }
+    }
+        
+    
 }
